340829e014692de9befae4b5e23fcfdff0eaf35c
hongyang <hongyang@asiainfo.com>
2019-12-11 17:17:28 +0800

    feat: init project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: sam.hongyang
+ * @LastEditors: sam.hongyang
+ * @Description: 用于注册相关日志任务
+ * @Date: 2019-12-11 16:52:14
+ * @LastEditTime: 2019-12-11 16:57:59
+ */
+
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: sam.hongyang
+ * @LastEditors: sam.hongyang
+ * @Description: webpack 相关配置
+ * @Date: 2019-12-11 17:14:42
+ * @LastEditTime: 2019-12-11 17:15:24
+ */
+module.exports = {
+
+}
